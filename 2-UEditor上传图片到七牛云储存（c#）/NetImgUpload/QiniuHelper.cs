94fb97fa06ea986458bb2cf1ba4a9321b07daaa2
ctxsdhy <ctxsdhy@foxmail.com>
2016-07-30 16:51:15 +0800

    2-UEditor上传图片到七牛云储存（c#）

---
+++ 2-UEditor上传图片到七牛云储存（c#）/NetImgUpload/QiniuHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using Qiniu.IO;
+using Qiniu.RS;
+
+namespace NetImgUpload
+{
+    /// <summary>
+    /// 七牛工具类
+    /// </summary>
+    public class QiniuHelper
+    {
+        /// <summary>
+        /// 空间名
+        /// </summary>
+        private static readonly string Scope = ConfigurationManager.AppSettings["QiniuScope"];
+
+        /// <summary>
+        /// 域名
+        /// </summary>
+        private static readonly string Url = ConfigurationManager.AppSettings["QiniuUrl"];
+
+        /// <summary>
+        /// 插入数据并返回交互结果
+        /// </summary>
+        /// <param name="imageFile"></param>
+        /// <returns></returns>
+        public static PutRet GetResult(byte[] imageFile)
+        {
+            var target = new IOClient();
+            var extra = new PutExtra
+            {
+                MimeType = "text/plain",
+                Crc32 = 123,
+                CheckCrc = CheckCrcType.CHECK,
+                Params = new Dictionary<string, string>()
+            };
+            var put = new PutPolicy(Scope);
+            return target.Put(put.Token(), Guid.NewGuid().ToString(), new MemoryStream(imageFile), extra);
+        }
+
+        /// <summary>
+        /// 获得url地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetUrl(string key)
+        {
+            return GetPolicy.MakeBaseUrl(Url, key);
+        }
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="key"></param>
+        public static void DeleteData(string key)
+        {
+            var client = new RSClient();
+            client.Delete(new EntryPath(Scope, key));
+        }
+
+        /// <summary>
+        /// 批量删除数据
+        /// </summary>
+        /// <param name="keys"></param>
+        public static void DeleteDatas(string keys)
+        {
+            var client = new RSClient();
+            var entryPaths = new List<EntryPath>();
+            foreach (string key in keys.Split(','))
+            {
+                entryPaths.Add(new EntryPath(Scope, key.Replace("'", "")));
+            }
+            client.BatchDelete(entryPaths.ToArray());
+        }
+    }
+}
---
+++ 2-UEditor上传图片到七牛云储存（c#）/NetImgUpload/UEditorPage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UEditorPage.aspx.cs" Inherits="NetImgUpload.UEditorPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>UEditor上传图片到七牛（.net）</title>
+    <script src="/UEditor/ueditor.config.js" type="text/javascript"></script>
+    <script src="/UEditor/ueditor.all.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <script type="text/plain" id="editor" name="editContent" style="width:600px; height:300px;"></script>
+        <script type="text/javascript">
+            var ue = UE.getEditor('editor');
+        </script>
+    </div>
+    </form>
+</body>
+</html>
