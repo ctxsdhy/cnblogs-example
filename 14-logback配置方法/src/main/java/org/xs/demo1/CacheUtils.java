0ad257c1128658d2668fe2e35b7973b4b5298163
ctxsdhy <ctxsdhy@foxmail.com>
2017-02-20 19:37:14 +0800

    14-logback配置方法

---
+++ 14-logback配置方法/src/main/java/org/xs/demo1/CacheUtils.java
@@ -0,0 +1,40 @@
+package org.xs.demo1;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+/**
+ * Cache工具类
+ */
+public class CacheUtils {
+	
+	private static CacheManager cacheManager = ((CacheManager)ContextUtils.getBean("ehcacheManager"));
+	
+	public static Object get(String cacheName, String key) {
+		Element element = getCache(cacheName).get(key);
+		return element==null?null:element.getObjectValue();
+	}
+
+	public static void put(String cacheName, String key, Object value) {
+		Element element = new Element(key, value);
+		getCache(cacheName).put(element);
+	}
+
+	public static void remove(String cacheName, String key) {
+		getCache(cacheName).remove(key);
+	}
+	
+	private static Cache getCache(String cacheName){
+		Cache cache = cacheManager.getCache(cacheName);
+		if (cache == null){
+			cacheManager.addCache(cacheName);
+			cache = cacheManager.getCache(cacheName);
+		}
+		return cache;
+	}
+
+	public static CacheManager getCacheManager() {
+		return cacheManager;
+	}
+}
---
+++ 14-logback配置方法/src/main/java/org/xs/demo1/ContextUtils.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright &copy; 2012-2013 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package org.xs.demo1;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+/**
+ * 获得ApplicaitonContext的工具.
+ */
+@Service
+@Lazy(false)
+public class ContextUtils implements ApplicationContextAware, DisposableBean {
+
+	private static ApplicationContext applicationContext = null;
+
+	/**
+	 * 取得存储在静态变量中的ApplicationContext.
+	 */
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	/**
+	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(String name) {
+		return (T) applicationContext.getBean(name);
+	}
+
+	/**
+	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+	 */
+	public static <T> T getBean(Class<T> requiredType) {
+		return applicationContext.getBean(requiredType);
+	}
+
+	/**
+	 * 实现ApplicationContextAware接口, 注入Context到静态变量中.
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		ContextUtils.applicationContext = applicationContext;
+	}
+
+	/**
+	 * 实现DisposableBean接口, 在Context关闭时清理静态变量.
+	 */
+	@Override
+	public void destroy() throws Exception {
+		applicationContext = null;
+	}
+}
